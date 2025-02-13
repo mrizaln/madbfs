23675901f0562b0631fb5fbb87a867f6291fe9f6
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2025-02-13 14:05:41 +0700

    Use conan to get libfuse as well

--- conanfile.py
+++ conanfile.py
@@ -4,7 +4,7 @@ from conan.tools.cmake import cmake_layout
 class Recipe(ConanFile):
     settings   = ["os", "compiler", "build_type", "arch"]
     generators = ["CMakeToolchain", "CMakeDeps"]
-    requires   = ["fmt/11.0.2", "spdlog/1.15.0"]
+    requires   = ["libfuse/3.16.2", "fmt/11.0.2", "spdlog/1.15.0"]
 
     def layout(self):
         cmake_layout(self)
