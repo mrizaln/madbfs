46309713e2b512d663670052671a466687fb9ff9
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2025-04-27 21:22:25 +0700

    Add Connection as abstraction for adb connection to device

---
+++ include/adbfsm/data/connection.hpp
@@ -0,0 +1,114 @@
+#pragma once
+
+#include "adbfsm/common.hpp"
+#include "adbfsm/data/stat.hpp"
+
+#include <generator>
+
+namespace adbfsm::path
+{
+    class Path;
+}
+
+namespace adbfsm::data
+{
+    struct ParsedStat
+    {
+        Stat stat;
+        Str  path;       // may contains escaped characters
+        Str  link_to;    // may be empty and may contains escaped characters
+    };
+
+    // NOTE: I made this interface to make Node and FileTree testable
+    class IConnection
+    {
+    public:
+        virtual Expect<std::generator<ParsedStat>> stat_dir(path::Path path)            = 0;
+        virtual Expect<ParsedStat>                 stat(path::Path path)                = 0;
+        virtual Expect<void>                       touch(path::Path path)               = 0;
+        virtual Expect<void>                       mkdir(path::Path path)               = 0;
+        virtual Expect<void>                       rm(path::Path path, bool recursive)  = 0;
+        virtual Expect<void>                       rmdir(path::Path path)               = 0;
+        virtual Expect<void>                       mv(path::Path from, path::Path to)   = 0;
+        virtual Expect<void>                       pull(path::Path from, path::Path to) = 0;
+        virtual Expect<void>                       push(path::Path from, path::Path to) = 0;
+
+    protected:
+        ~IConnection() = default;
+    };
+
+    class Connection : public IConnection
+    {
+    public:
+        /**
+         * @brief List
+         *
+         * @param path Path to a directory.
+         *
+         * @return A generator if successful, or an error if it fails.
+         */
+        Expect<std::generator<ParsedStat>> stat_dir(path::Path path) override;
+
+        /**
+         * @brief Get the stat of a file or directory.
+         *
+         * @param path The path to the file or directory.
+         *
+         * @return The stat of the file or directory.
+         */
+        Expect<ParsedStat> stat(path::Path path) override;
+
+        /**
+         * @brief Touch a file on the device.
+         *
+         * @param path Path to the file.
+         */
+        Expect<void> touch(path::Path path) override;
+
+        /**
+         * @brief Make a directory on the device.
+         *
+         * @param path Path to the directory.
+         */
+        Expect<void> mkdir(path::Path path) override;
+
+        /**
+         * @brief Remove a file on the device.
+         *
+         * @param path Path to the file on the device.
+         * @param bool Whether to remove recursively or not.
+         */
+        Expect<void> rm(path::Path path, bool recursive) override;
+
+        /**
+         * @brief Remove a directory on the device.
+         *
+         * @param path Path to the directory on the device.
+         */
+        Expect<void> rmdir(path::Path path) override;
+
+        /**
+         * @brief Move a file on the device.
+         *
+         * @param from Target file.
+         * @param to Destination file.
+         */
+        Expect<void> mv(path::Path from, path::Path to) override;
+
+        /**
+         * @brief Pull a file from the device to local.
+         *
+         * @param from Path to the file on the device.
+         * @param to Path to the file destination on local.
+         */
+        Expect<void> pull(path::Path from, path::Path to) override;
+
+        /**
+         * @brief Push a file from local to the device.
+         *
+         * @param from Path to the file on local.
+         * @param to Path to the file destination on the device.
+         */
+        Expect<void> push(path::Path from, path::Path to) override;
+    };
+}
---
+++ source/data/connection.cpp
@@ -0,0 +1,320 @@
+#include "adbfsm/log.hpp"
+#include "adbfsm/util.hpp"
+
+#include "adbfsm/data/connection.hpp"
+#include "adbfsm/path/path.hpp"
+
+#include <reproc++/drain.hpp>
+#include <reproc++/reproc.hpp>
+
+#include <grp.h>
+#include <pwd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+namespace
+{
+    using namespace adbfsm;
+
+    Expect<String> exec(Span<const Str> cmd)
+    {
+        using Sink = reproc::sink::string;
+
+        auto proc = reproc::process{};
+        auto args = reproc::arguments{ cmd };
+        auto opts = reproc::options{};
+
+        opts.redirect.err.type = reproc::redirect::pipe;
+
+        auto out = String{};
+        auto err = String{};
+
+        auto ec       = proc.start(args, opts);
+        auto ec_drain = reproc::drain(proc, Sink{ out }, Sink{ err });
+
+        if (ec != std::errc{}) {
+            const auto& errmsg = not err.empty() ? err : out;
+            log_e({ "failed to execute command [{}]: {}" }, cmd, errmsg);
+        }
+
+        if (ec_drain) {
+            return std::unexpected{ static_cast<std::errc>(ec_drain.value()) };
+        } else if (ec) {
+            return std::unexpected{ static_cast<std::errc>(ec.value()) };
+        }
+
+        return std::move(out);
+    }
+
+    mode_t parse_mode(Str str)
+    {
+        if (str.size() != 10) {
+            return 0;
+        }
+
+        auto fmode = mode_t{ 0 };
+
+        switch (str[0]) {
+        case 's': fmode |= S_IFSOCK; break;
+        case 'l': fmode |= S_IFLNK; break;
+        case '-': fmode |= S_IFREG; break;
+        case 'd': fmode |= S_IFDIR; break;
+        case 'b': fmode |= S_IFBLK; break;
+        case 'c': fmode |= S_IFCHR; break;
+        case 'p': fmode |= S_IFIFO; break;
+        }
+
+        fmode |= str[1] == 'r' ? S_IRUSR : 0;
+        fmode |= str[2] == 'w' ? S_IWUSR : 0;
+
+        switch (str[3]) {
+        case 'x': fmode |= S_IXUSR; break;
+        case 's': fmode |= S_ISUID | S_IXUSR; break;
+        case 'S': fmode |= S_ISUID; break;
+        }
+
+        fmode |= str[4] == 'r' ? S_IRGRP : 0;
+        fmode |= str[5] == 'w' ? S_IWGRP : 0;
+
+        switch (str[6]) {
+        case 'x': fmode |= S_IXGRP; break;
+        case 's': fmode |= S_ISGID | S_IXGRP; break;
+        case 'S': fmode |= S_ISGID; break;
+        }
+
+        fmode |= str[7] == 'r' ? S_IROTH : 0;
+        fmode |= str[8] == 'w' ? S_IWOTH : 0;
+
+        switch (str[9]) {
+        case 'x': fmode |= S_IXOTH; break;
+        case 't': fmode |= S_ISVTX | S_IXOTH; break;
+        case 'T': fmode |= S_ISVTX; break;
+        }
+
+        return fmode;
+    }
+
+    template <typename T>
+        requires std::is_fundamental_v<T>
+    constexpr Opt<T> parse_fundamental(Str str)
+    {
+        auto t         = T{};
+        auto [ptr, ec] = std::from_chars(str.data(), str.data() + str.size(), t);
+        if (ptr != str.data() + str.size() or ec != std::errc{}) {
+            return {};
+        }
+        return t;
+    }
+
+    Opt<time_t> parse_time(Str ymd, Str hmsms, Str offset)
+    {
+        // using std::chrono::parse function (the milliseconds part is not able to be parsed)
+        // parseable format: "2024-11-02 20:09:18 -0700"
+        auto format = "%F %T %z";
+        auto hms    = hmsms.substr(0, hmsms.find_first_of('.'));
+        auto buffer = std::stringstream{};
+
+        // clang-format off
+        for (auto ch : ymd)    { buffer << ch; } buffer << ' ';
+        for (auto ch : hms)    { buffer << ch; } buffer << ' ';
+        for (auto ch : offset) { buffer << ch; }
+        // clang-format on
+
+        auto time = Timestamp{};
+        if (buffer >> std::chrono::parse(format, time)) {
+            return Clock::to_time_t(time);
+        };
+
+        log_e({ "parse_time: failed to parse time [{} {} {}]" }, ymd, hms, offset);
+        return {};
+    }
+
+    /**
+     * @brief Parse the output of `ls -lla` command.
+     *
+     * @param str The output of `ls -lla` command.
+     * @param cache The cache to store the parsed stat.
+     * @param serial The serial number of the device.
+     *
+     * @return The parsed stat if successful, otherwise empty.
+     *
+     * example input:
+     *  -rw-rw----  1 root everybody 16037494 2025-02-02 03:50:34.000000000 +0700 pozy.qoi
+     *
+     * block device and character device are not supported currently:
+     *  crw-rw-rw-  1 root root        1,   5 2025-02-09 21:09:39.728000000 +0700 zero
+     *
+     * file with unknown stat field will not be parsed:
+     *  d?????????   ? ?      ?             ?                             ? metadata
+     *
+     * TODO: implement different parsing strategy for non-regular files
+     */
+    Opt<data::ParsedStat> parse_file_stat(Str str)
+    {
+        using namespace adbfsm;
+
+        auto result = util::split_n<8>(str, ' ');
+        if (not result) {
+            log_e({ "parse_file_stat: string can't be split into 8 parts [{}]" }, str);
+            return std::nullopt;
+        }
+
+        auto [to_be_stat, remainder] = *result;
+
+        auto path = remainder;
+        auto link = Str{};
+
+        if (auto arrow = remainder.find(" -> "); arrow != String::npos) {
+            path = remainder.substr(0, arrow);
+            link = remainder.substr(arrow + 4);
+        }
+
+        if (to_be_stat[0].find_first_of('?') != String::npos) {
+            log_e({ "parse_file_stat: failed, file contains unparsable data [{}]" }, to_be_stat[0]);
+            return std::nullopt;
+        }
+
+        auto name_store = std::array<char, 128>{};    // getpwnam & getgrpnam need null terminated string :(
+
+        name_store.fill(0);
+        std::copy(to_be_stat[2].begin(), to_be_stat[2].end(), name_store.data());
+        auto maybe_uid = ::getpwnam(name_store.data());
+
+        name_store.fill(0);
+        std::copy(to_be_stat[3].begin(), to_be_stat[3].end(), name_store.data());
+        auto maybe_grp = ::getgrnam(name_store.data());
+
+        // TODO: cache the uid and gid
+
+        auto stat = data::Stat{
+            .mode  = parse_mode(to_be_stat[0]),
+            .links = parse_fundamental<nlink_t>(to_be_stat[1]).value(),
+            .uid   = maybe_uid ? maybe_uid->pw_uid : 98,
+            .gid   = maybe_grp ? maybe_grp->gr_gid : 98,
+            .size  = parse_fundamental<off_t>(to_be_stat[4]).value_or(0),
+            .mtime = parse_time(to_be_stat[5], to_be_stat[6], to_be_stat[7]).value(),
+        };
+
+        if ((stat.mode & S_IFMT) == S_IFLNK and link.empty()) {
+            log_e({ "parse_file_stat: link is empty for [{}] when it should not be" }, path);
+        }
+
+        return data::ParsedStat{
+            .stat    = stat,
+            .path    = path,
+            .link_to = link,
+        };
+    }
+
+    // TODO: make this right
+    String resolve_path(Str parent, Str path)
+    {
+        auto resolved    = String{};
+        auto num_slashes = sr::count(parent | sv::drop(1), '/');
+
+        auto pos = 0uz;
+        while (path[pos] == '/') {
+            ++pos;
+        }
+        for (; num_slashes; --num_slashes) {
+            resolved.append("../");
+        }
+
+        resolved.append(path.data() + pos, path.size() - pos);
+        return resolved;
+    }
+}
+
+namespace adbfsm::data
+{
+    using namespace std::string_view_literals;
+
+    Expect<std::generator<ParsedStat>> Connection::stat_dir(path::Path path)
+    {
+        const auto cmd = Array{ "adb"sv, "shell"sv, "-lla"sv, path.fullpath() };
+
+        auto out = exec(cmd);
+        if (not out.has_value()) {
+            return std::unexpected{ out.error() };
+        }
+
+        auto generator = [out = std::move(out).value()] -> std::generator<ParsedStat> {
+            auto lines = util::StringSplitter{ out, '\n' };
+            while (auto line = lines.next()) {
+                auto parsed = parse_file_stat(util::strip(*line));
+                if (not parsed.has_value()) {
+                    log_e({ "Connection::stat_dir: parsing stat failed [{}]" }, *line);
+                    continue;
+                }
+                co_yield std::move(parsed).value();
+            }
+        };
+
+        return generator();
+    }
+
+    Expect<ParsedStat> Connection::stat(path::Path path)
+    {
+        const auto cmd = Array{ "adb"sv, "shell"sv, "-llad"sv, path.fullpath() };
+
+        auto out = exec(cmd);
+        if (not out.has_value()) {
+            return std::unexpected{ out.error() };
+        }
+
+        auto parsed = parse_file_stat(util::strip(*out));
+        if (not parsed.has_value()) {
+            log_e({ "Connection::stat: parsing stat failed [{}]" }, path.fullpath());
+            return std::unexpected{ std::errc::io_error };
+        }
+
+        return std::move(parsed).value();
+    }
+
+    Expect<void> Connection::touch(path::Path path)
+    {
+        const auto cmd = Array{ "adb"sv, "shell"sv, "touch"sv, path.fullpath() };
+        return exec(cmd).transform(sink_void);
+    }
+
+    Expect<void> Connection::mkdir(path::Path path)
+    {
+        const auto cmd = Array{ "adb"sv, "shell"sv, "mkdir"sv, path.fullpath() };
+        return exec(cmd).transform(sink_void);
+    }
+
+    Expect<void> Connection::rm(path::Path path, bool recursive)
+    {
+        if (not recursive) {
+            const auto cmd = Array{ "adb"sv, "shell"sv, "rm"sv, path.fullpath() };
+            return exec(cmd).transform(sink_void);
+        } else {
+            const auto cmd = Array{ "adb"sv, "shell"sv, "rm"sv, "-r"sv, path.fullpath() };
+            return exec(cmd).transform(sink_void);
+        }
+    }
+
+    Expect<void> Connection::rmdir(path::Path path)
+    {
+        const auto cmd = Array{ "adb"sv, "shell"sv, "rmdir"sv, path.fullpath() };
+        return exec(cmd).transform(sink_void);
+    }
+
+    Expect<void> Connection::mv(path::Path from, path::Path to)
+    {
+        const auto cmd = Array{ "adb"sv, "shell"sv, "mv"sv, from.fullpath(), to.fullpath() };
+        return exec(cmd).transform(sink_void);
+    }
+
+    Expect<void> Connection::pull(path::Path from, path::Path to)
+    {
+        const auto cmd = Array{ "adb"sv, "pull"sv, from.fullpath(), to.fullpath() };
+        return exec(cmd).transform(sink_void);
+    }
+
+    Expect<void> Connection::push(path::Path from, path::Path to)
+    {
+        const auto cmd = Array{ "adb"sv, "push"sv, from.fullpath(), to.fullpath() };
+        return exec(cmd).transform(sink_void);
+    }
+}
