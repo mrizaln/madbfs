1a3cee0c1c76f9ce20ead0ab1e4c7adb9d6a363e
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2025-05-16 02:18:17 +0700

    Add wrapper function for read_some and write_some

--- include/adbfsm/async/async.hpp
+++ include/adbfsm/async/async.hpp
@@ -3,6 +3,7 @@
 #include "adbfsm/async/as_expected.hpp"
 
 #include <boost/asio.hpp>
+#include <boost/asio/error.hpp>
 #include <boost/asio/experimental/awaitable_operators.hpp>
 
 #include <expected>
@@ -27,6 +28,53 @@ namespace adbfsm::async
         );
     }
 
+    inline std::errc to_generic_err(boost::system::error_code ec, std::errc fallback = std::errc::io_error)
+    {
+        if (ec.category() == boost::system::generic_category()) {
+            return static_cast<std::errc>(ec.value());
+        }
+        return static_cast<bool>(fallback) ? fallback : std::errc::invalid_argument;
+    }
+
+    template <typename AStream>
+    Await<std::pair<boost::system::error_code, std::size_t>> write_all(AStream& stream, std::string_view buf)
+    {
+        auto written = 0uz;
+        while (written < buf.size()) {
+            auto buffer = boost::asio::buffer(buf.data() + written, buf.size() - written);
+            auto res    = co_await stream.async_write_some(buffer);
+            if (not res) {
+                co_return std::pair{ res.error(), written };
+            }
+            auto len = *res;
+            if (len == 0) {
+                co_return std::pair{ std::make_error_code(std::errc::broken_pipe), written };
+            }
+            written += len;
+        }
+        co_return std::pair{ std::error_code{}, written };
+    }
+
+    template <typename AStream>
+    Await<std::pair<boost::system::error_code, std::size_t>> read_all(AStream& stream, std::string& buf)
+    {
+        auto dyn_buf = boost::asio::dynamic_buffer(buf);
+        while (true) {
+            auto res = co_await stream.async_read_some(dyn_buf);
+            if (res) {
+                continue;
+            }
+            if (res.error() == boost::asio::error::eof) {
+                break;
+            }
+            co_return std::pair{ res.error(), dyn_buf.size() };
+        }
+        co_return std::pair{ std::error_code{}, dyn_buf.size() };
+    }
+
+    using boost::asio::buffer;
+    using boost::asio::dynamic_buffer;
+
     namespace this_coro = boost::asio::this_coro;
     namespace operators = boost::asio::experimental::awaitable_operators;
 
@@ -35,5 +83,11 @@ namespace adbfsm::async
         using Proto    = boost::asio::local::stream_protocol;
         using Acceptor = Token::as_default_on_t<Proto::acceptor>;
         using Socket   = Token::as_default_on_t<Proto::socket>;
+    }
+
+    namespace pipe
+    {
+        using Write = Token::as_default_on_t<boost::asio::writable_pipe>;
+        using Read  = Token::as_default_on_t<boost::asio::readable_pipe>;
     }
 }
