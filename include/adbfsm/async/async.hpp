89e6f55448974b042d24044d9ec4cf2bccfc65c2
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2025-05-17 03:03:26 +0700

    Add new aliases and rename read/write_all to read/write_exact

--- include/adbfsm/async/async.hpp
+++ include/adbfsm/async/async.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "adbfsm/async/as_expected.hpp"
+#include "adbfsm/common.hpp"
 
 #include <boost/asio.hpp>
 #include <boost/asio/error.hpp>
@@ -9,71 +10,81 @@
 #include <expected>
 #include <utility>
 
+namespace adbfsm
+{
+    template <typename T>
+    using Await = boost::asio::awaitable<T>;
+
+    template <typename T>
+    using AExpect = Await<Expect<T>>;
+}
+
 namespace adbfsm::async
 {
     using Context  = boost::asio::io_context;
     using Token    = AsExpected<boost::asio::use_awaitable_t<>>;
     using Executor = Context::executor_type;
 
-    template <typename T>
-    using Await = boost::asio::awaitable<T>;
-
     template <typename Exec, typename Awaited, typename Completion>
     auto spawn(Exec&& ex, Awaited&& func, Completion&& completion)
     {
-        return co_spawn(
-            std::forward<Exec>(ex),    //
-            std::forward<Awaited>(func),
-            std::forward<Completion>(completion)
+        return boost::asio::co_spawn(
+            std::forward<Exec>(ex), std::forward<Awaited>(func), std::forward<Completion>(completion)
         );
     }
 
-    inline std::errc to_generic_err(boost::system::error_code ec, std::errc fallback = std::errc::io_error)
+    inline Errc to_generic_err(boost::system::error_code ec, Errc fallback = Errc::io_error)
     {
         if (ec.category() == boost::system::generic_category()) {
-            return static_cast<std::errc>(ec.value());
+            return static_cast<Errc>(ec.value());
         }
-        return static_cast<bool>(fallback) ? fallback : std::errc::invalid_argument;
+        return static_cast<bool>(fallback) ? fallback : Errc::invalid_argument;
     }
 
     template <typename AStream>
-    Await<std::pair<boost::system::error_code, std::size_t>> write_all(AStream& stream, std::string_view buf)
+    Await<Pair<boost::system::error_code, usize>> write_exact(AStream& stream, Span<const char> in)
     {
         auto written = 0uz;
-        while (written < buf.size()) {
-            auto buffer = boost::asio::buffer(buf.data() + written, buf.size() - written);
+        while (written < in.size()) {
+            auto buffer = boost::asio::buffer(in.data() + written, in.size() - written);
             auto res    = co_await stream.async_write_some(buffer);
             if (not res) {
-                co_return std::pair{ res.error(), written };
+                co_return Pair{ res.error(), written };
             }
             auto len = *res;
             if (len == 0) {
-                co_return std::pair{ std::make_error_code(std::errc::broken_pipe), written };
+                co_return Pair{ std::make_error_code(Errc::broken_pipe), written };
             }
             written += len;
         }
-        co_return std::pair{ std::error_code{}, written };
+        co_return Pair{ std::error_code{}, written };
     }
 
     template <typename AStream>
-    Await<std::pair<boost::system::error_code, std::size_t>> read_all(AStream& stream, std::string& buf)
+    Await<Pair<boost::system::error_code, usize>> read_exact(AStream& stream, Span<char> out)
     {
-        auto dyn_buf = boost::asio::dynamic_buffer(buf);
-        while (true) {
-            auto res = co_await stream.async_read_some(dyn_buf);
-            if (res) {
-                continue;
+        auto read = 0uz;
+        while (read < out.size()) {
+            auto buffer = boost::asio::buffer(out.data() + read, out.size() - read);
+            auto res    = co_await stream.async_read_some(buffer);
+            if (not res) {
+                co_return Pair{ res.error(), read };
             }
-            if (res.error() == boost::asio::error::eof) {
-                break;
+            auto len = *res;
+            if (len == 0) {
+                co_return Pair{ std::make_error_code(Errc::broken_pipe), read };
             }
-            co_return std::pair{ res.error(), dyn_buf.size() };
+            read += len;
         }
-        co_return std::pair{ std::error_code{}, dyn_buf.size() };
+        co_return Pair{ std::error_code{}, read };
     }
 
     using boost::asio::buffer;
     using boost::asio::dynamic_buffer;
+
+    using boost::asio::detached;
+    using boost::asio::use_awaitable;
+    using boost::asio::use_future;
 
     namespace this_coro = boost::asio::this_coro;
     namespace operators = boost::asio::experimental::awaitable_operators;
@@ -81,6 +92,7 @@ namespace adbfsm::async
     namespace unix_socket
     {
         using Proto    = boost::asio::local::stream_protocol;
+        using Endpoint = Proto::endpoint;
         using Acceptor = Token::as_default_on_t<Proto::acceptor>;
         using Socket   = Token::as_default_on_t<Proto::socket>;
     }
