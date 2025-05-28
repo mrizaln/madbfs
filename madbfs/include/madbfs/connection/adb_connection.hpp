ecd95b8e5d4e3fab88973839bfd4ae6237c772d0
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2025-05-28 19:05:44 +0700

    Move docstring to Connection interface

--- madbfs/include/madbfs/connection/adb_connection.hpp
+++ madbfs/include/madbfs/connection/adb_connection.hpp
@@ -9,123 +9,22 @@ namespace madbfs::connection
     public:
         AdbConnection() = default;
 
-        // directory operations
-        // --------------------
-
-        /**
-         * @brief List
-         *
-         * @param path Path to a directory.
-         *
-         * @return A generator if successful, or an error if it fails.
-         */
         AExpect<Gen<ParsedStat>> statdir(path::Path path) override;
+        AExpect<data::Stat>      stat(path::Path path) override;
+        AExpect<path::PathBuf>   readlink(path::Path path) override;
 
-        /**
-         * @brief Get the stat of a file or directory.
-         *
-         * @param path The path to the file or directory.
-         *
-         * @return The stat of the file or directory.
-         */
-        AExpect<data::Stat> stat(path::Path path) override;
-
-        /**
-         * @brief Get the real file pointed by a symlink.
-         *
-         * @param path The path to the file or directory.
-         */
-        AExpect<path::PathBuf> readlink(path::Path path) override;
-
-        /**
-         * @brief Create a new empty file.
-         *
-         * @param path Path of the new file.
-         */
         AExpect<void> mknod(path::Path path) override;
-
-        /**
-         * @brief Make a directory on the device.
-         *
-         * @param path Path to the directory.
-         */
         AExpect<void> mkdir(path::Path path) override;
-
-        /**
-         * @brief Remove a file on the device.
-         *
-         * @param path Path to the file on the device.
-         * @param bool Whether to remove recursively or not.
-         */
         AExpect<void> unlink(path::Path path) override;
-
-        /**
-         * @brief Remove a directory on the device.
-         *
-         * @param path Path to the directory on the device.
-         */
         AExpect<void> rmdir(path::Path path) override;
-
-        /**
-         * @brief Move a file on the device.
-         *
-         * @param from Target file.
-         * @param to Destination file.
-         */
         AExpect<void> rename(path::Path from, path::Path to) override;
 
-        // --------------------
-
-        // file operations
-        // ---------------
-
-        /**
-         * @brief Truncate a file on the device.
-         *
-         * @param path Path to the file on the device.
-         * @param size Size to truncate to.
-         */
-        AExpect<void> truncate(path::Path path, off_t size) override;
-
-        /**
-         * @brief Read from a file on the device.
-         *
-         * @param path Path to the file on the device.
-         * @param out Buffer to read into.
-         * @param offset Offset to read from.
-         */
+        AExpect<void>  truncate(path::Path path, off_t size) override;
         AExpect<usize> read(path::Path path, Span<char> out, off_t offset) override;
-
-        /**
-         * @brief Write to a file on the device.
-         *
-         * @param path Path to the file on the device.
-         * @param in Buffer to write from.
-         * @param offset Offset to write to.
-         */
         AExpect<usize> write(path::Path path, Span<const char> in, off_t offset) override;
+        AExpect<void>  utimens(path::Path path, timespec atime, timespec mtime) override;
 
-        /**
-         * @brief Update change time and modification time of a file
-         *
-         * @param path Path to the file on the device.
-         * @param atime Access time.
-         * @param mtime Modification time.
-         */
-        AExpect<void> utimens(path::Path path, timespec atime, timespec mtime) override;
-
-        /**
-         * @brief Copy file server-side.
-         *
-         * @param in Input file path.
-         * @param in_off Input offset.
-         * @param out Output file path.
-         * @param out_off Output offset.
-         * @param size Number of bytes to be copied.
-         */
         AExpect<usize> copy_file_range(path::Path in, off_t in_off, path::Path out, off_t out_off, usize size)
             override;
-
-        // ---------------
     };
 }
--- madbfs/include/madbfs/connection/connection.hpp
+++ madbfs/include/madbfs/connection/connection.hpp
@@ -21,23 +21,120 @@ namespace madbfs::connection
     class Connection
     {
     public:
-        virtual AExpect<Gen<ParsedStat>> statdir(path::Path path)  = 0;
-        virtual AExpect<data::Stat>      stat(path::Path path)     = 0;
-        virtual AExpect<path::PathBuf>   readlink(path::Path path) = 0;
-
         // directory operations
-        virtual AExpect<void> mknod(path::Path path)                 = 0;
-        virtual AExpect<void> mkdir(path::Path path)                 = 0;
-        virtual AExpect<void> unlink(path::Path path)                = 0;
-        virtual AExpect<void> rmdir(path::Path path)                 = 0;
+        // --------------------
+
+        /**
+         * @brief List
+         *
+         * @param path Path to a directory.
+         *
+         * @return A generator if successful, or an error if it fails.
+         */
+        virtual AExpect<Gen<ParsedStat>> statdir(path::Path path) = 0;
+
+        /**
+         * @brief Get the stat of a file or directory.
+         *
+         * @param path The path to the file or directory.
+         *
+         * @return The stat of the file or directory.
+         */
+        virtual AExpect<data::Stat> stat(path::Path path) = 0;
+
+        /**
+         * @brief Get the real file pointed by a symlink.
+         *
+         * @param path The path to the file or directory.
+         */
+        virtual AExpect<path::PathBuf> readlink(path::Path path) = 0;
+
+        /**
+         * @brief Create a new empty file.
+         *
+         * @param path Path of the new file.
+         */
+        virtual AExpect<void> mknod(path::Path path) = 0;
+
+        /**
+         * @brief Make a directory on the device.
+         *
+         * @param path Path to the directory.
+         */
+        virtual AExpect<void> mkdir(path::Path path) = 0;
+
+        /**
+         * @brief Remove a file on the device.
+         *
+         * @param path Path to the file on the device.
+         * @param bool Whether to remove recursively or not.
+         */
+        virtual AExpect<void> unlink(path::Path path) = 0;
+
+        /**
+         * @brief Remove a directory on the device.
+         *
+         * @param path Path to the directory on the device.
+         */
+        virtual AExpect<void> rmdir(path::Path path) = 0;
+
+        /**
+         * @brief Move a file on the device.
+         *
+         * @param from Target file.
+         * @param to Destination file.
+         */
         virtual AExpect<void> rename(path::Path from, path::Path to) = 0;
 
-        // file operations
-        virtual AExpect<void>  truncate(path::Path path, off_t size)                     = 0;
-        virtual AExpect<usize> read(path::Path path, Span<char> out, off_t offset)       = 0;
-        virtual AExpect<usize> write(path::Path path, Span<const char> in, off_t offset) = 0;
-        virtual AExpect<void>  utimens(path::Path path, timespec atime, timespec mtime)  = 0;
+        // --------------------
 
+        // file operations
+        // ---------------
+
+        /**
+         * @brief Truncate a file on the device.
+         *
+         * @param path Path to the file on the device.
+         * @param size Size to truncate to.
+         */
+        virtual AExpect<void> truncate(path::Path path, off_t size) = 0;
+
+        /**
+         * @brief Read from a file on the device.
+         *
+         * @param path Path to the file on the device.
+         * @param out Buffer to read into.
+         * @param offset Offset to read from.
+         */
+        virtual AExpect<usize> read(path::Path path, Span<char> out, off_t offset) = 0;
+
+        /**
+         * @brief Write to a file on the device.
+         *
+         * @param path Path to the file on the device.
+         * @param in Buffer to write from.
+         * @param offset Offset to write to.
+         */
+        virtual AExpect<usize> write(path::Path path, Span<const char> in, off_t offset) = 0;
+
+        /**
+         * @brief Update change time and modification time of a file
+         *
+         * @param path Path to the file on the device.
+         * @param atime Access time.
+         * @param mtime Modification time.
+         */
+        virtual AExpect<void> utimens(path::Path path, timespec atime, timespec mtime) = 0;
+
+        /**
+         * @brief Copy file server-side.
+         *
+         * @param in Input file path.
+         * @param in_off Input offset.
+         * @param out Output file path.
+         * @param out_off Output offset.
+         * @param size Number of bytes to be copied.
+         */
         virtual AExpect<usize> copy_file_range(
             path::Path in,
             off_t      in_off,
@@ -45,6 +142,8 @@ namespace madbfs::connection
             off_t      out_off,
             usize      size
         ) = 0;
+
+        // ---------------
 
         virtual ~Connection() = default;
     };
