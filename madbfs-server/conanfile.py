8b6f836ff0b2e2deb3af43b01268b9d353c76868
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2025-06-04 08:43:02 +0700

    Move spdlog, fmt, and saf dependencies to common submodule

--- madbfs-server/conanfile.py
+++ madbfs-server/conanfile.py
@@ -5,7 +5,11 @@ from conan.tools.cmake import cmake_layout
 class Recipe(ConanFile):
     settings = ["os", "compiler", "build_type", "arch"]
     generators = ["CMakeToolchain", "CMakeDeps"]
-    requires = ["asio/1.29.0", "spdlog/1.15.1"]
+    requires = [
+        "asio/1.29.0",
+        "fmt/11.1.3",
+        "spdlog/1.15.1",
+    ]
 
     def layout(self):
         cmake_layout(self)
--- madbfs/cmake/fetched-libs.cmake
+++ madbfs/cmake/fetched-libs.cmake
@@ -12,14 +12,3 @@ FetchContent_MakeAvailable(linr)
 
 add_library(fetch::linr ALIAS linr)
 # ------------
-
-# saf library
-# ------------
-FetchContent_Declare(
-  saf
-  GIT_REPOSITORY https://github.com/ashtum/saf
-  GIT_TAG 142c7e987d779babf0a895eda05faca231ed918f)
-FetchContent_MakeAvailable(saf)
-
-add_library(fetch::saf ALIAS saf)
-# ------------
